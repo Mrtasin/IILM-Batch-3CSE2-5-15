39114490e6e42cfdc68b5f538dc61b01842977d1
Mrtasin <mohammadtasinspj@gmail.com>
2026-02-26 10:13:53 +0530

    Implement Queue using Stacks

--- ImplementQueueusingStacks.py
+++ ImplementQueueusingStacks.py
@@ -1,18 +1,34 @@
+class Node:
+    def __init__(self, data = None):
+        self.value = data
+        self.next = None
 
 class MyQueue:
 
     def __init__(self):
+        self.start = self.end = None
 
     def push(self, x: int) -> None:
-
+        temp = Node(x)
+        if self.start == None:
+            self.start = self.end = temp
+        else :
+            self.end.next = temp
+            self.end = temp
 
     def pop(self) -> int:
+        data = self.start.value
+        if self.start.next == None:
+            self.start = self.end = None
+        else:
+            self.start = self.start.next
+        return data
 
     def peek(self) -> int:
-
+        return self.start.value
 
     def empty(self) -> bool:
-
+        return (self.start == None and self.end == None)
         
 
 
